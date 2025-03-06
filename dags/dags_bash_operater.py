a85bb4e3d4455b27c727bfbb4c269cfc63e0aa07
Minhyuck <102458641+architectureDelyn@users.noreply.github.com>
2025-03-06 21:37:32 +0900

    email operator 업로드

--- dags/dags_bash_operater.py
+++ dags/dags_bash_operater.py
@@ -5,7 +5,7 @@ import datetime
 import pendulum
 
 with DAG(
-    dag_id="dags_bash_operatore",
+    dag_id="dags_bash_operator",
     schedule="0 0 * * *",
     start_date=pendulum.datetime(2021, 1, 1, tz="Asia/Seoul"),
     catchup=False,
